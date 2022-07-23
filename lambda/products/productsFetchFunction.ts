ed8703c1d576a8f4e3f52f142a090662e60bbf40
Igor Monteiro <igornathandev@gmail.com>
2022-07-23 14:11:55 -0300

    feat: add GET /products/{id} in productsFetchFunction

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -18,6 +18,16 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
         })
       }
     }
+  } else if (event.resource === "/products/{id}") {
+      const productId = event.pathParameters!.id as string
+      console.log(`GET /products/${productId}`)
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `GET /products/${productId}`
+        })
+      }
   }
 
   return {
