7607f281a1e42149432dd1a972b60520ae93f9d0
Igor Monteiro <igornathandev@gmail.com>
2022-07-24 01:15:35 -0300

    refactor: associate correct value to productsAdminHandler

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -23,7 +23,7 @@ const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
 
 const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", {
   productsFetchHandler: productsAppStack.productsFetchHandler,
-  productsAdminHandler: productsAppStack.productsFetchHandler,
+  productsAdminHandler: productsAppStack.productsAdminHandler,
   tags,
   env
 })
